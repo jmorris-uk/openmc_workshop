91e630b26d51a9d81deaf51c4f8e4c9aeb99c200
Jonathan Shimwell <Jonathan.Shimwell@ukaea.uk>
2019-01-23 18:28:37 +0000

    corrected 3d tokamak plot

--- tasks/task_2/4_example_geometry_viewer_3d.py
+++ tasks/task_2/4_example_geometry_viewer_3d.py
@@ -18,19 +18,29 @@ mats.export_to_xml()
 
 
 #example surfaces
-sph1 = openmc.Sphere(R=10, boundary_type='vacuum')
-sph2 = openmc.Sphere(R=20, boundary_type='vacuum')
-sph3 = +sph1 & -sph2 #above sph1 and below sph2
+sph1 = openmc.Sphere(R=10)
+sph2 = openmc.Sphere(R=20)
 
-#add surfaces here using https://openmc.readthedocs.io/en/stable/usersguide/geometry.html#surfaces-and-regions
+core_of_sphere = -sph1 # volume is below sph1
+hollow_sphere = +sph1 & -sph2 # volume is above sph1 and below sph2
 
-#example cell
-cell = openmc.Cell(region=sph3)
-cell.fill = natural_lead
+#hint, add cylindical surfaces here using https://openmc.readthedocs.io/en/stable/usersguide/geometry.html#surfaces-and-regions
 
-#add another cell here
 
-universe = openmc.Universe(cells=[cell]) #this list will need to include the new cell
+#hint, this is an example cell, by default it is filled with a vacuum
+cell1 = openmc.Cell(region=core_of_sphere)
+
+cell2 = openmc.Cell(region=hollow_sphere)
+cell2.fill = natural_lead
+
+
+
+
+#add more cells here
+
+universe = openmc.Universe(cells=[cell1, cell2]) #this list will need to include the new cell
+
+
 
 geom = openmc.Geometry(universe)
 
@@ -38,7 +48,7 @@ geom.export_to_xml()
 
 vox_plot = openmc.Plot()
 vox_plot.type = 'voxel'
-vox_plot.width = (20., 20., 20.)
+vox_plot.width = (40., 40., 40.)
 vox_plot.pixels = (200, 200, 200)
 vox_plot.filename = 'plot_3d'
 vox_plot.color_by = 'material'
